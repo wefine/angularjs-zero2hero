5ecfe3497eefb8d022fafc9d948da0916bc30450
wefine <wang.xiaoren@zte.com.cn>
2017-02-21 23:37:24 +0800

    15 - Directive to Directive Communication
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -1,20 +1,61 @@
+// this code has been updated to not
+// use $scope! It now uses the "controller as"
+// syntax instead.
 import angular from "angular";
 
 export default angular.module('heroApp', [])
-    .controller("AppCtrl", function () {
-        let vm = this;
-        vm.loadMoreTweets = function () {
-            alert("Loading tweets!");
+    .controller("SuperHeroCtrl", function ($element) {
+        // controller declared on module so it can use "controller as"
+        let superman = this;
+
+        superman.abilities = [];
+
+        superman.addStrength = function () {
+            superman.abilities.push("strength");
         };
 
-        vm.deleteTweets = function () {
-            alert("deleting tweets");
+        superman.addSpeed = function () {
+            superman.abilities.push("speed");
+        };
+
+        superman.addFlight = function () {
+            superman.abilities.push("flight");
+        };
+
+        $element.addClass("button");
+        $element.bind("click", function () {
+            console.log(superman.abilities);
+        });
+    })
+    .directive("superHero", function () {
+        return {
+            restrict: "E",
+            scope: {},
+
+            controller: "SuperHeroCtrl as superhero"
         };
     })
-    .directive("enter", function () {
-        return function (scope, element, attrs) {
-            element.bind("mouseenter", function () {
-                scope.$apply(attrs.enter);
-            });
+    .directive("strength", function () {
+        return {
+            require: "superHero",
+            link: function (scope, element, attrs, superheroCtrl) {
+                superheroCtrl.addStrength();
+            }
+        };
+    })
+    .directive("speed", function () {
+        return {
+            require: "superHero",
+            link: function (scope, element, attrs, superheroCtrl) {
+                superheroCtrl.addSpeed();
+            }
+        };
+    })
+    .directive("flight", function () {
+        return {
+            require: "superHero",
+            link: function (scope, element, attrs, superheroCtrl) {
+                superheroCtrl.addFlight();
+            }
         };
     });
