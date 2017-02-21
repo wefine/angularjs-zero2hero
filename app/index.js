d6174b68bff7771c552417002dc09a648b9d233e
wefine <wang.xiaoren@zte.com.cn>
2017-02-21 21:54:10 +0800

    06 - Filters
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -3,16 +3,19 @@ import angular from "angular";
 export default angular.module('z2hApp', [])
     .factory('Data', function () {
         return {
-            message: "I'm data from a service"
+            message: "Message"
         };
     })
-    .controller('FirstCtrl', function ($scope, Data) {
-        $scope.data = Data;
-    })
-    .controller('SecondCtrl', function ($scope, Data) {
-        $scope.data = Data;
-
-        $scope.reversedMessage = function (message) {
-            return message.split("").reverse().join("");
+    .filter('reverse', function (Data) {
+        return function(text) {
+            return text.split("").reverse().join("") + Data.message;
         };
+    })
+    .controller('FirstCtrl', function (Data) {
+        let vm = this;
+        vm.data = Data;
+    })
+    .controller('SecondCtrl', function (Data) {
+        let vm = this;
+        vm.data = Data;
     });
