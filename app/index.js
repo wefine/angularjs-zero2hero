261ed698cf2567f4523af9936a64aa6f154856a8
wefine <wang.xiaoren@zte.com.cn>
2017-02-21 23:16:20 +0800

    14 - Directives Talking to Controllers
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -1,17 +1,20 @@
 import angular from "angular";
 
 export default angular.module('heroApp', [])
+    .controller("AppCtrl", function () {
+        let vm = this;
+        vm.loadMoreTweets = function () {
+            alert("Loading tweets!");
+        };
+
+        vm.deleteTweets = function () {
+            alert("deleting tweets");
+        };
+    })
     .directive("enter", function () {
         return function (scope, element, attrs) {
             element.bind("mouseenter", function () {
-                element.addClass(attrs.enter);
-            });
-        };
-    })
-    .directive("leave", function () {
-        return function (scope, element, attrs) {
-            element.bind("mouseleave", function () {
-                element.removeClass(attrs.enter);
+                scope.$apply(attrs.enter);
             });
         };
     });
