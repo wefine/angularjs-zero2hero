0c977e2a1a7c66eec06db33a6892a5ea631a0ef7
wefine <wang.xiaoren@zte.com.cn>
2017-02-21 22:36:15 +0800

    10 - First Directive
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -1,73 +1,9 @@
 import angular from "angular";
 
-export default angular.module('z2hApp', [])
-    .factory('Avengers', function () {
-        let Avengers = {};
-        Avengers.cast = [
-            {
-                name: "Robert Downey Jr.",
-                character: "Tony Stark / Iron Man"
-            },
-            {
-                name: "Chris Evans",
-                character: "Steve Rogers / Captain America"
-            },
-            {
-                name: "Mark Ruffalo",
-                character: "Bruce Banner / The Hulk"
-            },
-            {
-                name: "Chris Hemsworth",
-                character: "Thor"
-            },
-            {
-                name: "Scarlett Johansson",
-                character: "Natasha Romanoff / Black Widow"
-            },
-            {
-                name: "Jeremy Renner",
-                character: "Clint Barton / Hawkeye"
-            },
-            {
-                name: "Tom Hiddleston",
-                character: "Loki"
-            },
-            {
-                name: "Clark Gregg",
-                character: "Agent Phil Coulson"
-            },
-            {
-                name: "Cobie Smulders",
-                character: "Agent Maria Hill"
-            },
-            {
-                name: "Stellan Skarsgard",
-                character: "Selvig"
-            },
-            {
-                name: "Samuel L. Jackson",
-                character: "Nick Fury"
-            },
-            {
-                name: "Gwyneth Paltrow",
-                character: "Pepper Potts"
-            },
-            {
-                name: "Paul Bettany",
-                character: "Jarvis (voice)"
-            },
-            {
-                name: "Alexis Denisof",
-                character: "The Other"
-            },
-            {
-                name: "Tina Benko",
-                character: "NASA Scientist"
-            }
-        ];
-        return Avengers;
-    })
-    .controller('AvengersCtrl', function (Avengers) {
-        let vm = this;
-        vm.avengers = Avengers;
+export default angular.module('heroApp', [])
+    .directive("superman", function() {
+        return {
+            restrict: "E",
+            template: "<div>Here I am to save the day</div>"
+        };
     });
