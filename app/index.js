2e097c86466faa2428cf2a156756884bc8f311ff
wefine <wang.xiaoren@zte.com.cn>
2017-02-22 22:49:16 +0800

    22 - An Alternative Approach to Controllers
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -1,12 +1,10 @@
 import angular from "angular";
 
 export default angular.module('heroApp', [])
-    .controller("AppCtrl", function ($scope) {
-    })
-    .directive("panel", function () {
-        return {
-            restrict: "E",
-            transclude: true,
-            template: '<div class="panel" ng-transclude>This is a panel component</div>'
+    .controller("AppCtrl", function() {
+        let ctrl = this;
+
+        ctrl.sayHi = function() {
+            alert("hi");
         };
     });
