fa9bfdaedbfa8d0ad1057a6b4c29e6fba796f3dd
wefine <wang.xiaoren@zte.com.cn>
2017-02-22 16:07:47 +0800

    16 - Understanding Isolate Scope
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -1,61 +1,20 @@
-// this code has been updated to not
-// use $scope! It now uses the "controller as"
-// syntax instead.
 import angular from "angular";
 
 export default angular.module('heroApp', [])
-    .controller("SuperHeroCtrl", function ($element) {
-        // controller declared on module so it can use "controller as"
-        let superman = this;
-
-        superman.abilities = [];
-
-        superman.addStrength = function () {
-            superman.abilities.push("strength");
+    .controller("ChoreCtrl", function () {
+        let vm = this;
+        vm.logChore = function (chore) {
+            alert(chore + " is done!");
         };
-
-        superman.addSpeed = function () {
-            superman.abilities.push("speed");
-        };
-
-        superman.addFlight = function () {
-            superman.abilities.push("flight");
-        };
-
-        $element.addClass("button");
-        $element.bind("click", function () {
-            console.log(superman.abilities);
-        });
     })
-    .directive("superHero", function () {
+    .directive("kid", function () {
         return {
             restrict: "E",
-            scope: {},
-
-            controller: "SuperHeroCtrl as superhero"
-        };
-    })
-    .directive("strength", function () {
-        return {
-            require: "superHero",
-            link: function (scope, element, attrs, superheroCtrl) {
-                superheroCtrl.addStrength();
-            }
-        };
-    })
-    .directive("speed", function () {
-        return {
-            require: "superHero",
-            link: function (scope, element, attrs, superheroCtrl) {
-                superheroCtrl.addSpeed();
-            }
-        };
-    })
-    .directive("flight", function () {
-        return {
-            require: "superHero",
-            link: function (scope, element, attrs, superheroCtrl) {
-                superheroCtrl.addFlight();
-            }
+            scope: {
+                done: "&"
+            },
+            template: '<input type="text" ng-model="value">' +
+            ' {{value}}' +
+            ' <div class="button" ng-click="done({chore:value})">I\'m done!</div>'
         };
     });
