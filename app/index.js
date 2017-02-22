26d9ccf22f4f9a95d4a49d5777ebaa5ed35b2ac0
wefine <wang.xiaoren@zte.com.cn>
2017-02-22 22:32:50 +0800

    21 - Transclusion Basics
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -2,30 +2,11 @@ import angular from "angular";
 
 export default angular.module('heroApp', [])
     .controller("AppCtrl", function ($scope) {
-        $scope.leaveVoicemail = function (number, message) {
-            alert("Number: " + number + " said: " + message);
-        };
     })
-    .directive("phone", function () {
+    .directive("panel", function () {
         return {
             restrict: "E",
-            scope: {
-                number: "@",
-                network: "=",
-                makeCall: "&"
-            },
-            template:
-            '<div class="panel">' +
-                'Number: {{number}} Network:' +
-                '<select ng-model="network" ng-options="net for net in networks">' +
-                '<input type="text" ng-model="value">' +
-                '<div class="button" ng-click="makeCall({number: number, message: value})">' +
-                    'Call home!' +
-                '</div>' +
-            '</div>',
-            link: function (scope) {
-                scope.networks = ["Verizon", "AT&T", "Sprint"];
-                scope.network = scope.networks[0];
-            }
+            transclude: true,
+            template: '<div class="panel" ng-transclude>This is a panel component</div>'
         };
     });
