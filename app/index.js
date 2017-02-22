79500e3031247f28764937b4968b0574c51fb8fc
wefine <wang.xiaoren@zte.com.cn>
2017-02-22 19:01:11 +0800

    20 - Isolate Scope Review
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -1,18 +1,31 @@
 import angular from "angular";
 
 export default angular.module('heroApp', [])
-    .controller("AppCtrl", function () {
-        var appctrl = this;
-        appctrl.callHome = function (message) {
-            alert(message);
+    .controller("AppCtrl", function ($scope) {
+        $scope.leaveVoicemail = function (number, message) {
+            alert("Number: " + number + " said: " + message);
         };
     })
     .directive("phone", function () {
         return {
+            restrict: "E",
             scope: {
-                dial: "&"
+                number: "@",
+                network: "=",
+                makeCall: "&"
             },
-            template: '<input type="text" ng-model="value">' +
-            '<div class="button" ng-click="dial({message:value})">Call home!</div>'
+            template:
+            '<div class="panel">' +
+                'Number: {{number}} Network:' +
+                '<select ng-model="network" ng-options="net for net in networks">' +
+                '<input type="text" ng-model="value">' +
+                '<div class="button" ng-click="makeCall({number: number, message: value})">' +
+                    'Call home!' +
+                '</div>' +
+            '</div>',
+            link: function (scope) {
+                scope.networks = ["Verizon", "AT&T", "Sprint"];
+                scope.network = scope.networks[0];
+            }
         };
     });
