ede16a5d03ca475b2d9ce33f7c8ed6d8d9fa718b
wefine <wang.xiaoren@zte.com.cn>
2017-02-23 23:28:52 +0800

    add bootstrap, vendor.js, etc.
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,24 @@ const path = require('path');
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
 
 module.exports = {
     context: __dirname + '/app',
-    entry: './index.js',
+    entry: {
+        app: [
+            // TODO: doesn't work functionality, may fix it in the future.
+            // 'bootstrap-loader/lib/bootstrap.loader?extractStyles&configFilePath='+__dirname+'/.bootstraprc!bootstrap-loader/no-op.js',
+            './index.js'
+        ],
+        vendor: [
+            'bootstrap',
+            'angular'
+        ]
+    },
     output: {
         path: __dirname + '/app',
-        filename: 'bundle.[hash].js'
+        filename: '[hash].bundle.js'
     },
     module: {
         rules: [
@@ -16,19 +27,42 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
-            }, {
+            },
+            {
                 test: /\.html$/,
                 loader: 'raw-loader'
-            }, {
+            },
+            {
                 test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                    fallbackLoader: 'style-loader',
-                    loader: [
-                        {loader: 'css-loader', query: {sourceMap: true}},
-                        {loader: 'postcss-loader'}
-                    ],
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ['style-loader', 'css-loader', 'resolve-url-loader']
                 })
-            }
+            },
+            {
+                test: /\.png$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file"
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
+            },
         ]
     },
     plugins: [
@@ -40,6 +74,12 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery",
             "window.jQuery": "jquery"
+        }),
+        new ExtractTextPlugin('styles.css'),
+        new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: '[hash].vendor.js'}),
+        new AddAssetHtmlPlugin({
+            filepath: require.resolve('./node_modules/bootstrap/dist/css/bootstrap.css'),
+            typeOfAsset: 'css'
         })
     ],
     devServer: {
