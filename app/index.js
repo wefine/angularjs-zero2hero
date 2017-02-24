173560a8c16f6dc2cc22273a3a3e73a08f055afb
wefine <wang.xiaoren@zte.com.cn>
2017-02-24 23:05:39 +0800

    28 - $templateCache
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -3,6 +3,9 @@ import "./footer.css";
 import angular from "angular";
 
 export default angular.module('heroApp', [])
+    .run(function($templateCache) {
+        $templateCache.put("zippy.html", '<div><h3 data-ng-click="toggleContent()">{{title}}</h3><div data-ng-show="isContentVisible" data-ng-transclude></div></div>');
+    })
     .controller('MyCtrl', ['$scope', function ($scope) {
         $scope.model = {};
         $scope.model.content = "default";
