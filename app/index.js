0c2cdfaf0468b112e1c0b1bacc4d6967a7ceff2b
wefine <wang.xiaoren@zte.com.cn>
2017-02-25 02:41:26 +0800

    34 - Promises, $q.defer.promise
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -2,28 +2,32 @@ import angular from "angular";
 
 export default angular.module('heroApp', ["ngRoute"])
     .config(function ($routeProvider) {
-
         $routeProvider
             .when('/', {
                 templateUrl: "app.html",
                 controller: "AppCtrl"
             })
-            .when('/pizza/:crust/:toppings', {
-                redirectTo: function (routeParams, path, search) {
-                    console.log(routeParams);
-                    console.log(path);
-                    console.log(search);
-                    return "/" + routeParams.crust;
-                }
-            })
-            .when('/deep', {
-                template: 'Deep dish'
-            })
-            .otherwise({
-                redirectTo: "/"
-            })
     })
-    .controller("AppCtrl", function ($scope, $routeParams) {
+    .controller("AppCtrl", function ($scope, $q) {
+        let defer = $q.defer();
+
+        defer.promise
+            .then(function (weapon) {
+                alert("You can have my " + weapon);
+
+                return "bow";
+            })
+            .then(function (weapon) {
+                alert("And my " + weapon);
+
+                return "axe";
+            })
+            .then(function (weapon) {
+                alert("And my " + weapon);
+            });
+
+        defer.resolve("sword");
+
         $scope.model = {
             message: "This is my app!!!"
         }
