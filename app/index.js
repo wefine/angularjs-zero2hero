d0b92af9e8760f1f6d5c5ca46bcf5b39fdc38d8c
wefine <wang.xiaoren@zte.com.cn>
2017-02-25 02:50:09 +0800

    35 - Resolve
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -5,29 +5,21 @@ export default angular.module('heroApp', ["ngRoute"])
         $routeProvider
             .when('/', {
                 templateUrl: "app.html",
-                controller: "AppCtrl"
+                controller: "AppCtrl",
+                resolve: {
+                    app: function ($q, $timeout) {
+                        let defer = $q.defer();
+
+                        $timeout(function () {
+                            defer.resolve();
+                        }, 10000);
+
+                        return defer.promise;
+                    }
+                }
             })
     })
     .controller("AppCtrl", function ($scope, $q) {
-        let defer = $q.defer();
-
-        defer.promise
-            .then(function (weapon) {
-                alert("You can have my " + weapon);
-
-                return "bow";
-            })
-            .then(function (weapon) {
-                alert("And my " + weapon);
-
-                return "axe";
-            })
-            .then(function (weapon) {
-                alert("And my " + weapon);
-            });
-
-        defer.resolve("sword");
-
         $scope.model = {
             message: "This is my app!!!"
         }
