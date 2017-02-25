523233b34d42dc26051f616ca48e8a8f4ad3fc2d
wefine <wang.xiaoren@zte.com.cn>
2017-02-25 16:31:42 +0800

    51 - angular.copy
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -1,30 +1,32 @@
 import angular from "angular";
 
 angular.module("heroApp", ["ngAnimate"])
-    .controller("AppCtrl", function () {
-        this.isHidden = false;
-        this.fadeIt = function () {
-            this.isHidden = !this.isHidden;
-        };
+    .factory("contacts", function () {
+        return [
+            {"firstName": "Angelica", "lastName": "Britt", "phone": "513-0955"},
+            {"firstName": "Amery", "lastName": "Compton", "phone": "1-417-395-4214"},
+            {"firstName": "Wendy", "lastName": "Morrow", "phone": "294-8234"},
+            {"firstName": "Mercedes", "lastName": "Merrill", "phone": "1-206-670-0109"},
+            {"firstName": "Porter", "lastName": "Anderson", "phone": "817-4745"},
+            {"firstName": "Leah", "lastName": "Melendez", "phone": "406-3746"},
+            {"firstName": "Olga", "lastName": "Mcgowan", "phone": "1-567-304-7633"},
+        ];
     })
-    .directive("hideMe", function ($animate) {
-        return function (scope, element, attrs) {
-            scope.$watch(attrs.hideMe, function (newVal) {
-                if (newVal) {
-                    $animate.addClass(element, "fade");
-                } else {
-                    $animate.removeClass(element, "fade");
-                }
-            });
+    .controller("AppCtrl", function (contacts) {
+        let ctrl = this;
+
+        ctrl.contacts = contacts;
+        ctrl.selectedContact = null;
+        ctrl.contactCopy = null;
+
+        ctrl.selectContact = function (contact) {
+            ctrl.selectedContact = contact;
+            ctrl.contactCopy = angular.copy(contact);
         };
-    })
-    .animation(".fade", function () {
-        return {
-            addClass: function (element, className) {
-                TweenMax.to(element, 1, {opacity: 0});
-            },
-            removeClass: function (element, className) {
-                TweenMax.to(element, 1, {opacity: 1});
+
+        ctrl.saveContact = function () {
+            if (ctrl.selectedContact != null) {
+                ctrl.selectedContact.firstName = ctrl.contactCopy.firstName;
             }
         };
     });
