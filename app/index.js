adccaccf0d9a583923080b5c6d399402e91a9bff
wefine <wang.xiaoren@zte.com.cn>
2017-02-25 14:00:39 +0800

    45 - Directive Communication
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -1,11 +1,38 @@
-import 'foundation'
+import "foundation";
 import angular from "angular";
 
 angular.module("heroApp", [])
-    .controller("RoomCtrl", function () {
-        this.openDoor = function () {
-            alert("creak");
+    .directive("country", function () {
+        return {
+            restrict: "E",
+            controller: function () {
+                this.makeAnnouncement = function (message) {
+                    console.log("Country says: " + message);
+                };
+            }
         };
+    })
+    .directive("state", function () {
+        return {
+            restrict: "E",
+            require: "^country",
+            controller: function () {
+                this.makeLaw = function (law) {
+                    console.log("Law: " + law);
+                };
+            },
+            link: function (scope, element, attrs, countryCtrl) {
 
-        this.buttonTitle = "I'm a button";
+            }
+        };
+    })
+    .directive("city", function () {
+        return {
+            restrict: "E",
+            require: ["^country", "^state"],
+            link: function (scope, element, attrs, ctrls) {
+                ctrls[0].makeAnnouncement("from city");
+                ctrls[1].makeLaw("Jump higher");
+            }
+        };
     });
