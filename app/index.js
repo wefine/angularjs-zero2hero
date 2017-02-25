9adc97f7e39645e269d27926167aaf75165374c4
wefine <wang.xiaoren@zte.com.cn>
2017-02-25 14:06:22 +0800

    47 - ng-repeat-start
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -2,37 +2,6 @@ import "foundation";
 import angular from "angular";
 
 angular.module("heroApp", [])
-    .directive("country", function () {
-        return {
-            restrict: "E",
-            controller: function () {
-                this.makeAnnouncement = function (message) {
-                    console.log("Country says: " + message);
-                };
-            }
-        };
-    })
-    .directive("state", function () {
-        return {
-            restrict: "E",
-            require: "^country",
-            controller: function () {
-                this.makeLaw = function (law) {
-                    console.log("Law: " + law);
-                };
-            },
-            link: function (scope, element, attrs, countryCtrl) {
-
-            }
-        };
-    })
-    .directive("city", function () {
-        return {
-            restrict: "E",
-            require: ["^country", "^state"],
-            link: function (scope, element, attrs, ctrls) {
-                ctrls[0].makeAnnouncement("from city");
-                ctrls[1].makeLaw("Jump higher");
-            }
-        };
+    .controller("AppCtrl", function() {
+        this.things = ["one", "two", "three"];
     });
