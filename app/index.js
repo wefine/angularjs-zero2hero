afea8ccfcf917d40c3cb7790ee804f1cecdf26ac
wefine <wang.xiaoren@zte.com.cn>
2017-02-26 20:12:22 +0800

    53 - Using angular.bootstrap to Initialize Your App
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- app/index.js
+++ app/index.js
@@ -1,43 +1,9 @@
 import angular from "angular";
 
 angular.module("heroApp", ["ui.router"])
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('home', {
-                url: '/home',
-                templateUrl: 'templates/home.html'
-            })
-            .state('list', {
-                url: '/list',
-                templateUrl: 'templates/list.html',
-                controller: 'ListCtrl'
-            })
-            .state('list.item', {
-                url: '/:item',
-                templateUrl: 'templates/list.item.html',
-                controller: function ($scope, $stateParams) {
-                    $scope.item = $stateParams.item;
-                }
-            });
-
-        $urlRouterProvider.otherwise('/home');
-    })
-    .controller("ListCtrl", function ($scope) {
-        $scope.shoppingList = [
-            {name: 'Milk'},
-            {name: 'Eggs'},
-            {name: 'Bread'},
-            {name: 'Cheese'},
-            {name: 'Ham'}
-        ];
-
-        $scope.selectItem = function (selectedItem) {
-            // lodash
-            _($scope.shoppingList).each(function (item) {
-                item.selected = false;
-                if (selectedItem === item) {
-                    selectedItem.selected = true;
-                }
-            });
-        };
+    .run(function ($rootScope) {
+        $rootScope.random = Math.random();
     });
+
+angular.bootstrap(document.getElementById("container"), ["heroApp"])
+angular.bootstrap(document.getElementById("container2"), ["heroApp"]);
